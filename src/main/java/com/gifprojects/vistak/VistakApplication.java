6d77aff035827b8ec85a19eb9b5a799bdcbf640b
Gaita Iustin Florian <iustingaita.f@gmail.com>
2026-03-19 10:30:48 +0200

    first commit: setup spring boot app

---
+++ src/main/java/com/gifprojects/vistak/VistakApplication.java
@@ -0,0 +1,13 @@
+package com.gifprojects.vistak;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VistakApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VistakApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gifprojects/vistak/VistakApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gifprojects.vistak;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VistakApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
