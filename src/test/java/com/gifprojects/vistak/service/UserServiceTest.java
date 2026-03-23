51cc408a14eadcc18532e4c7c19e65b3e7f0f497
Gaita Iustin Florian <iustingaita.f@gmail.com>
2026-03-23 12:41:23 +0200

    refactor: UserServiceMockTest -> UserServiceTest
    todo: implement JWT (JSON Web Tokens); test REST APIs with Postman

--- src/test/java/com/gifprojects/vistak/service/UserServiceTest.java
+++ src/test/java/com/gifprojects/vistak/service/UserServiceTest.java
@@ -21,7 +21,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
-public class UserServiceMockTest {
+public class UserServiceTest {
     @Mock
     private UserRepository userRepositoryMock;
 
